ba8395a940e4f917c6552816c6ac1dbf93b5db2c
sick <matteo.franzolin@iisviolamarchesini.edu.it>
2022-03-14 18:30:52 +0100

    iterazioniDefiniteIndefinite

---
+++ iterazioni.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+int i=0;
+int j=0;
+for(int i=0; i<10; i++){
+printf("TPSIT\n");}
+do{
+j++;
+printf("TPSIT\n");
+}while(j<10);
+while(i<10){
+i++;
+printf("TPSIT\n");}
+return 0;
+}
