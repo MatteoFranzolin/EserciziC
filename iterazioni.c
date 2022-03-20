87deda7939c0c10aff57952bf4adf86e284f2cf1
sick <sick@DESKTOP-DLNTCCO.localdomain>
2022-03-20 18:45:36 +0100

    IfElse Giorni

--- iterazioni.c
+++ iterazioni.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
-
-int main(){
-int i=0;
-int j=0;
-for(int i=0; i<10; i++){
-printf("TPSIT\n");}
-do{
-j++;
-printf("TPSIT\n");
-}while(j<10);
-while(i<10){
-i++;
-printf("TPSIT\n");}
-return 0;
+int main()
+{ 
+   int numero;
+   scanf("%d", &numero);
+   if(numero==1){
+      printf("Lunedì\n"); 
+   }
+   else if(numero==2){
+      printf("Martedì\n"); 
+   }
+   else if(numero==3){
+      printf("Mercoledì\n"); 
+   }
+   else if(numero==4){
+      printf("Giovedì\n"); 
+   }
+   else if(numero==5){
+      printf("Venerdì\n"); 
+   }
+   else if(numero==6){
+      printf("Sabato\n"); 
+   }
+   else if(numero==7){
+      printf("Domenica\n"); 
+   }
+   else{
+       printf("Giorno non valido\n"); 
+   }
+   return 0;
 }
