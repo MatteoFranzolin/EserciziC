9ba4a90b587c38c2d656e723fff4eec68569da18
Matteo <matteo.franzolin@iisviolamarchesini.edu.it>
2022-03-28 17:38:07 +0200

    OperatoreTernario

--- fileC.c
+++ fileC.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 { 
-   int num1 = 9;
-   int num2 = 3;
-   printf("AND: %d\n", (num1&num2)); //risultato: 1
-   printf("OR: %d\n", (num1|num2)); //risultato: 11
-   printf("XOR: %d\n", (num1^num2)); //risultato: 10
-   return 0;
+   int voto;
+   scanf("%d", &voto);
+   (voto>=0) ? printf("Il voto è positivo\n") //se la condizione è vera allora esegue la prima opzione
+             : printf("Il voto è negativo\n"); //se è falsa esegue la seconda
+             //può essere utilizzato al posto di if e else
+   return 0; 
 }
