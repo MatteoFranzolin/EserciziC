b6932ac39dd462326e0e61cb4e22a99e8a539405
Matteo <matteo.franzolin@iisviolamarchesini.edu.it>
2022-04-02 00:10:03 +0200

    stampa array prima pari

--- fileC.c
+++ fileC.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 int main()
 { 
-   int voto;
-   scanf("%d", &voto);
-   (voto>=0) ? printf("Il voto è positivo\n") //se la condizione è vera allora esegue la prima opzione
-             : printf("Il voto è negativo\n"); //se è falsa esegue la seconda
-             //può essere utilizzato al posto di if e else
+   int input;
+   int i;
+   int array[10];
+   for(i=0; i<10; i++){
+      scanf("%d", &array[i]);
+   }
+   for(i=0; i<10; i++){
+         if(array[i]%2==0){
+          printf("%d / ", array[i]);
+         }
+      }
+   for(i=0; i<10; i++){
+         if(array[i]%2!=0){
+          printf("%d / ", array[i]);
+         }
+      }
    return 0; 
 }
